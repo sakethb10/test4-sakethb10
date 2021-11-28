e088e3c9d03a377995622ee744e0059c22f62679
sakethb10 <93110587+sakethb10@users.noreply.github.com>
2021-11-28 15:23:23 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main()
 {
-  printf("enter numbers/n");
-  
+  int a,b,c;
+  printf("enter two numbers: ");
+  scanf("%d%d", &a, &b );
+  c=a+b;
+  printf("%d + %d is %d\n", a,b,c);
+  return 0;
+}
