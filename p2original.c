f4b57a89720794cf2476e05283b1c98e309d086b
sakethb10 <93110587+sakethb10@users.noreply.github.com>
2021-12-07 10:21:59 +0530

    Update p2original.c

--- p2original.c
+++ p2original.c
@@ -23,3 +23,30 @@ void output(int a, intb, int sum)
 {
   printf("%d + %d is %d\n", a,b,c);
 }
+
+
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d", &a);
+  return a;
+}
+int add (int a, int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output (int a, int b, int c)
+{
+printf("%d+%d=%d\n", a, b, c);
+}
+int main()
+{
+  int a= input ();
+  int b= input ();
+  int c= add (a,b);
+  output (a,b,c);
+}
