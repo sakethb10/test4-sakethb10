d040a70755f28da25d1d56a66c40ea644c5c02fc
sakethb10 <93110587+sakethb10@users.noreply.github.com>
2021-12-12 17:33:45 +0530

    Update p1final.c

--- p1final.c
+++ p1final.c
@@ -2,9 +2,9 @@
 int main()
 {
   int a,b,c;
-  printf("enter two numbers: ");
-  scanf("%d%d", &a, &b );
+  printf("Enter two values to be added:\n ");
+  scanf("%d%d", &a, &b);
   c=a+b;
-  printf("%d + %d is %d\n", a,b,c);
+  printf("Sum of %d + %d is %d\n", a,b,c);
   return 0;
 }
