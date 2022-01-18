b149771229b32d63cde299228c36b9e19d660f6c
sakethb10 <93110587+sakethb10@users.noreply.github.com>
2022-01-18 20:49:49 +0530

    Update p5afinal.c

--- p5afinal.c
+++ p5afinal.c
@@ -27,11 +27,11 @@ void output(int a, int b, int c, int s)
 }
 int main()
 {
-  int r,p,t,s=0;
+  int r,p,t,s;
   r = input();
   p = input();
   t = input();
-  s = cmp(r,t,p);
-  output(r,t,p,s);
+  s = cmp(r,p,t);
+  output(r,p,t,s);
   return 0;
 }
